8e20cacda34b2ae341900b71c000cef8cde9568b
moiseevanton <athidif94@gmail.com>
2016-05-11 16:11:06 -0400

    Initial Commit

---
+++ Mafia mini-games/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mafia mini-games
+//
+//  Created by Anton Moiseev on 2016-05-11.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
